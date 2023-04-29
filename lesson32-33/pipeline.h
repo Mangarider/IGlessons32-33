182a8f422db3e21a0d6e064c4564ad93723cac84
Mangarider <101807484+Mangarider@users.noreply.github.com>
2023-04-29 10:23:07 +0500

    Добавьте файлы проекта.

---
+++ lesson32-33/pipeline.h
@@ -0,0 +1,127 @@
+/*
+    Copyright 2010 Etay Meiri
+
+    This program is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+*/
+
+#ifndef PIPELINE_H
+#define	PIPELINE_H
+
+#include "math_3d.h"
+
+class Pipeline
+{
+public:
+    Pipeline()
+    {
+        m_scale = Vector3f(1.0f, 1.0f, 1.0f);
+        m_worldPos = Vector3f(0.0f, 0.0f, 0.0f);
+        m_rotateInfo = Vector3f(0.0f, 0.0f, 0.0f);
+    }
+
+    void Scale(float ScaleX, float ScaleY, float ScaleZ)
+    {
+        m_scale.x = ScaleX;
+        m_scale.y = ScaleY;
+        m_scale.z = ScaleZ;
+    }
+
+    void WorldPos(float x, float y, float z)
+    {
+        m_worldPos.x = x;
+        m_worldPos.y = y;
+        m_worldPos.z = z;
+    }
+
+    void WorldPos(const Vector3f& Pos)
+    {
+        m_worldPos = Pos;
+    }
+
+    void Rotate(float RotateX, float RotateY, float RotateZ)
+    {
+        m_rotateInfo.x = RotateX;
+        m_rotateInfo.y = RotateY;
+        m_rotateInfo.z = RotateZ;
+    }
+
+    void SetPerspectiveProj(const PersProjInfo& p)
+    {
+        m_persProjInfo = p;
+    }
+
+    void SetCamera(const Vector3f& Pos, const Vector3f& Target, const Vector3f& Up)
+    {
+        m_camera.Pos = Pos;
+        m_camera.Target = Target;
+        m_camera.Up = Up;
+    }
+
+    const Matrix4f& GetVPTrans();
+    const Matrix4f& GetWVPTrans();
+    const Matrix4f& GetWorldTrans();
+
+private:
+    Vector3f m_scale;
+    Vector3f m_worldPos;
+    Vector3f m_rotateInfo;
+
+    PersProjInfo m_persProjInfo;
+
+    struct {
+        Vector3f Pos;
+        Vector3f Target;
+        Vector3f Up;
+    } m_camera;
+
+    Matrix4f m_WVPtransformation;
+    Matrix4f m_VPTtransformation;
+    Matrix4f m_WorldTransformation;
+};
+
+const Matrix4f& Pipeline::GetVPTrans()
+{
+    Matrix4f CameraTranslationTrans, CameraRotateTrans, PersProjTrans;
+
+    CameraTranslationTrans.InitTranslationTransform(-m_camera.Pos.x, -m_camera.Pos.y, -m_camera.Pos.z);
+    CameraRotateTrans.InitCameraTransform(m_camera.Target, m_camera.Up);
+    PersProjTrans.InitPersProjTransform(m_persProjInfo);
+
+    m_VPTtransformation = PersProjTrans * CameraRotateTrans * CameraTranslationTrans;
+    return m_VPTtransformation;
+}
+
+const Matrix4f& Pipeline::GetWorldTrans()
+{
+    Matrix4f ScaleTrans, RotateTrans, TranslationTrans;
+
+    ScaleTrans.InitScaleTransform(m_scale.x, m_scale.y, m_scale.z);
+    RotateTrans.InitRotateTransform(m_rotateInfo.x, m_rotateInfo.y, m_rotateInfo.z);
+    TranslationTrans.InitTranslationTransform(m_worldPos.x, m_worldPos.y, m_worldPos.z);
+
+    m_WorldTransformation = TranslationTrans * RotateTrans * ScaleTrans;
+    return m_WorldTransformation;
+}
+
+const Matrix4f& Pipeline::GetWVPTrans()
+{
+    GetWorldTrans();
+    GetVPTrans();
+
+    m_WVPtransformation = m_VPTtransformation * m_WorldTransformation;
+    return m_WVPtransformation;
+}
+
+#endif	/* PIPELINE_H */
+
